821d43b5a37d70bfa331cc5e3570945f259a413e
Howard Zhang <44009661+dos320@users.noreply.github.com>
2019-12-12 10:11:00 -0500

    multiplayer support
    colour switching, rendering etc.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-var http = require("http");
 const express = require("express");
-const app = express();
+const app = require('express')();
+var http = require("http").createServer(app);
+const io = require('socket.io')(http);
+
+
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080, () => {
+io.on("connection", (socket)=>{
+  /*
+    TODO:
+    - drawing history - maybe use an array or something to store all the points that were drawn
+    - draw point - receive points and draw lines based on those points
+  */
+  console.log("test");
+  socket.on("send point", (point)=>{
+    // emits the point for all clients except the sender to render, client-side
+    socket.broadcast.emit("draw line", point);
+  });
+
+  socket.on("stop drawing line", ()=>{
+    socket.broadcast.emit("stop drawing line");
+  });
+
+  socket.on("colour change", (colour)=>{
+    socket.broadcast.emit("colour change", colour);
+  });
+  
+});
+
+http.listen(8080, () => {
   console.log("now listening on port 8080.");
 });
